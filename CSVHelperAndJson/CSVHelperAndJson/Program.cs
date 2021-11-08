a27af6d43fbb3297c8ddaf03d2eb545f7334fc32
b-s-ad <ahmedshehbazb@gmail.com>
2021-11-08 21:52:54 +0530

    Saving and Reading the Address Book from CSV and JSON

---
+++ CSVHelperAndJson/CSVHelperAndJson/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CSVHelperAndJson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("# Read data from CSV & Write data in CSV");
+           // CSVHandler.ImplementCSVDataHandling();
+            ReadCSV_And_WriteJSON.ImplementCSVToJSON();
+
+            Console.ReadKey();
+        }
+    }
+}
